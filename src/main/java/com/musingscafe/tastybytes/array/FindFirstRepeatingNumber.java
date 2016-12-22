948367ed9395ae2a9501c68da5c2afde246013ac
ayadav <anujyadavcse@gmail.com>
2016-12-22 18:42:46 +0530

    added one more implementation for finding first repeating number in an array

--- src/main/java/com/musingscafe/tastybytes/array/FindFirstRepeatingNumber.java
+++ src/main/java/com/musingscafe/tastybytes/array/FindFirstRepeatingNumber.java
@@ -1,11 +1,16 @@
 package com.musingscafe.tastybytes.array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by ayadav on 12/21/16.
  */
 public class FindFirstRepeatingNumber {
     public static void main(String[] args) {
+
         findFirstRepeatingNumberBruteForce(new int[] {2,4,5,6,2,3});
+        findFirstRepeatingNumberMap(new int[] {2,4,5,6,2,3});
     }
 
     private static void findFirstRepeatingNumberBruteForce(int[] array) {
@@ -17,6 +22,21 @@ public class FindFirstRepeatingNumber {
                     return;
                 }
             }
+        }
+
+        System.out.println("No repeating number");
+    }
+
+    private static void findFirstRepeatingNumberMap(int[] array) {
+        int length = array.length;
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < length; i++) {
+            if (map.containsKey(array[i])) {
+                System.out.println(array[i]);
+                return;
+            }
+
+            map.put(array[i], 1);
         }
 
         System.out.println("No repeating number");
